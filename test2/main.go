d7ff6a4731c77d1105403e4bba94404a9328465d
Kimyeonho <kyh0703@nate.com>
2023-07-18 09:32:10 +0900

    Revert "[Feature] Update"
    This reverts commit b266d4cceb05db778c51fca69fab68abdc9dd8a4.

--- test2/main.go
+++ test2/main.go
@@ -1,29 +1,126 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+const MaxEvent = 30
+
+type Result struct {
+	eventType uint16
+	err       error
+	time      time.Time
+}
+
+type Handler struct {
+	id       uuid.UUID
+	timer    *time.Timer
+	bus      chan uint16
+	expectID string
+	close    chan struct{}
+}
+
+func NewHandler() *Handler {
+	uuid, _ := uuid.NewV4()
+
+	return &Handler{
+		id:    uuid,
+		bus:   make(chan uint16, MaxEvent),
+		close: make(chan struct{}, 1), // Buffered to avoid locking up the event feed
+	}
+}
+
+func (h *Handler) ID() uuid.UUID              { return h.id }
+func (h *Handler) SetTimer(dur time.Duration) { h.timer = time.NewTimer(dur) }
+func (h *Handler) ExpectID() string           { return h.expectID }
+func (h *Handler) SetExpectID(id string)      { h.expectID = id }
+
+func (h *Handler) Close() {
+	close(h.bus)
+	if h.close != nil {
+		close(h.close)
+	}
+	h.bus = nil
+	h.close = nil
+}
+
+func (h *Handler) Wait(ctx context.Context, callID string) <-chan Result {
+	eventStream := make(chan Result)
+	go func() {
+		defer close(eventStream)
+		defer h.Close()
+
+		// set default timer
+		if h.timer == nil {
+			h.timer = time.NewTimer(time.Second * 20)
+		}
+		// waiting event
+		for {
+			select {
+			case event := <-h.bus:
+				fmt.Println("bus input", event)
+				eventStream <- Result{eventType: event}
+			case <-h.timer.C:
+				fmt.Println("event handler timeout")
+				eventStream <- Result{err: errors.New("timeout")}
+				return
+			case <-h.close:
+				fmt.Println("event close")
+				eventStream <- Result{err: nil}
+				return
+			case <-ctx.Done():
+				fmt.Println("ctx done")
+				return
+			}
+		}
+	}()
+	return eventStream
+}
 
 func main() {
-	// var wg sync.WaitGroup
-	// wg.Add(1)
-	// timer := time.NewTimer(time.Second * 10)
-	// go func() {
-	// 	<-timer.C
-	// 	fmt.Println("timer ON")
-	// 	u, ok := service.UserCache.Get("621c1055a89c8e6470dbb194")
-	// 	fmt.Println(u.ID, u.Tenant, ok)
-	// 	wg.Done()
-	// }()
-	// wg.Add(1)
-	// go service.StartHook()
-	// wg.Wait()
+// 	eh := NewHandler()
 
-	maps := make(map[string]int)
-	maps["test"] = 1
-	fmt.Println(maps)
+// 	go func() {
+// 		select {
+// 		case <-time.After(time.Second * 4):
+// 			eh.bus <- 11
+// 		}
+// 	}()
 
-	mapOfMap := make(map[string]map[string]int)
-	inner := make(map[string]int)
-	mapOfMap["test"] = inner
-	mapOfMap["test"]["test"] = 1
-	fmt.Println(mapOfMap)
+// 	go func() {
+// 		select {
+// 		case <-time.After(time.Second * 8):
+// 			eh.bus <- 13
+// 		}
+// 	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+// 	stream := eh.Wait(ctx, "12345")
+// loop:
+// 	select {
+// 	case result := <-stream:
+// 		if result.err != nil {
+// 			fmt.Println("error is not null", result.err)
+// 			return
+// 		}
+// 		fmt.Println("no error event type:", result)
+// 		goto loop
+// 	case <-ctx.Done():
+// 		fmt.Println("context error")
+// 		return
+// 	}
+
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		fmt.Println("hhhh")
+	}
 }
--- test3/go.mod
+++ test3/go.mod
@@ -1,8 +1,12 @@
 module test3
 
-go 1.18
+go 1.17
 
 replace (
 	event => ./event
 	kafkaevent => ./event/kafkaevent
 )
+
+require event v0.0.0-00010101000000-000000000000
+
+require kafkaevent v0.0.0-00010101000000-000000000000 // indirect
